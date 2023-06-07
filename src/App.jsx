18343658bc9c46b226b19903c52b1a024787b166
John Vick <woram.j.vic@gmail.com>
2023-06-07 17:55:25 +0700

    return values implemented > applying input.target.value

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,18 @@ import './App.css'
 function App() {
   //state
   const [time, setTime] = useState(new Date())
-  const [inputDate, setInputDate] = useState('1 Jan 2023');
-  const [birthDate, setBirthDate] = useState(new Date("2023-12-06").getTime());
+  const defalutDate = "1 Jan 1111"
+  const [inputDate, setInputDate] = useState(false);
+  const [birthDate, setBirthDate] = useState(new Date(defalutDate).getTime());
+  const [result, setResult] = useState({ days: 0, hours: 0, minutes: 0, seconds: 0 })
+
   //Handler
   const onChangeHandler = (e) => {
     setInputDate(e.target.value);
   }
   const countDown = () => {
-    setBirthDate(new Date(inputDate));
+    inputDate ? setBirthDate(new Date(inputDate).getTime()) : alert("please enter your birthday")
+    console.log("calculated the days")
   }
 
   //function input TimeStamp and then return day,hours,minutes and seconds
@@ -31,16 +35,17 @@ function App() {
     let seconds = Math.floor(timeCount % 60)
 
     console.log("day...", days, "hrs...", hours, "min...", minutes, "sec...", seconds)
-    return [days, hours, minutes, seconds]
+    return { days, hours, minutes, seconds }
   }
-  const { days, hours, minutes, seconds } = getCountDown()
-  const [result, setResult] = useState({ day: days, hour: hours, minute: minutes, second: seconds })
 
   useEffect(() => {
     const intervalTask = setInterval(() => {
       setTime(new Date())
-      getCountDown(birthDate)
-      setResult({ day: days, hour: hours, minute: minutes, second: seconds })
+      const data = getCountDown(birthDate)
+      setResult({ ...data });
+      // const { days, hours, minutes, seconds } = getCountDown()
+      // setResult({ day: days, hour: hours, minute: minutes, second: seconds })
+
     }, 1000) //1000 millisecond = 1 second
     return () => clearInterval(intervalTask)
   }, [])
@@ -50,10 +55,10 @@ function App() {
       <h1>Countdown</h1>
       <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, aliquam ipsam ipsum iure possimus sint quia culpa doloremque nihil recusandae?</h4>
       <div className='countdown'>
-        <div>{result.day}</div>
-        <div>{result.hour}</div>
-        <div>{result.minute}</div>
-        <div>{result.second}</div>
+        <div>{result.days}</div>
+        <div>{result.hours}</div>
+        <div>{result.minutes}</div>
+        <div>{result.seconds}</div>
 
         {/* <div >{days}</div>
         <div>{hours}</div>
